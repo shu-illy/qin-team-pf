f4ef397767ad8654a84a7cad6ff48ddd6350fc0f
shu-illy <sh.illy.9117@gmail.com>
2022-08-08 01:39:37 +0900

    Mantine, TailwindCSS併用の設定_2

---
+++ src/lib/mantine/Button.tsx
@@ -0,0 +1,18 @@
+import type { ButtonProps } from "@mantine/core";
+import { cloneElement, forwardRef } from "react";
+import { Button as MantineButton } from "@mantine/core";
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps & { dent?: boolean }>(
+  ({ sx, dent, ...props }, ref) => {
+    return cloneElement(<MantineButton />, {
+      sx: {
+        ...sx,
+        "&:not(:disabled):active": dent ? undefined : { transform: "none" },
+      },
+      ref,
+      ...props,
+    });
+  }
+);
+
+Button.displayName = "Button";
