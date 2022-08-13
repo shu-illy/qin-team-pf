8ea956c71b792e901cbd871929abec3cedbf9ff6
shu-illy <sh.illy.9117@gmail.com>
2022-08-14 02:41:49 +0900

    使用言語のProgressのコンポーネント作成

--- src/types/GithubRepository.ts
+++ src/types/GithubRepository.ts
@@ -1,8 +1,12 @@
+import { Language } from "types";
+
+export type Languages = { name: Language; value: number }[];
+
 export type GithubRepository = {
   id: number;
   title: string;
   description: string;
   star: number;
   fork: number;
-  languages: { [language: string]: number };
+  languages: Languages;
 };
