ecb95177e2d22b05fe8e614682d6d97a17b3af13
shu-illy <sh.illy.9117@gmail.com>
2022-08-14 04:48:53 +0900

    ポートフォリオのレスポンシブ対応修正

--- src/components/organisms/Portfolios.tsx
+++ src/components/organisms/Portfolios.tsx
@@ -2,6 +2,7 @@ import { Center, Grid, Space, Stack } from "@mantine/core";
 import SectionBottomButton from "components/atoms/SectionBottomButton";
 import { SectionTitle } from "components/atoms/SectionTitle";
 import PortfolioItem from "components/molecules/PortfolioItem";
+import { useMediaQuery } from "lib/mantine";
 import React from "react";
 import { Portfolio } from "types";
 
@@ -11,13 +12,14 @@ type Props = {
 };
 
 export const Portfolios: React.FC<Props> = ({ portfolios, isAll }) => {
+  const isDesktop = useMediaQuery("sm");
   const items = isAll ? portfolios : portfolios.slice(0, 6);
   return (
     <Stack spacing={0}>
       <SectionTitle>Portfolio</SectionTitle>
       <Grid gutter={24}>
         {items.map((portfolio) => (
-          <Grid.Col key={portfolio.id} span={4}>
+          <Grid.Col key={portfolio.id} span={isDesktop ? 4 : 12}>
             <PortfolioItem portfolio={portfolio} />
           </Grid.Col>
         ))}
