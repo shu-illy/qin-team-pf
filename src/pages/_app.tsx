aa66758e0c1582047c7631b1b7dcdd172a672b69
shu-illy <sh.illy.9117@gmail.com>
2022-08-14 06:19:42 +0900

    ライト/ダークモードをlocalStrageに保存、ショートカットキーを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "lib/tailwind.css";
 import type { AppProps } from "next/app";
 import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
-import { useState } from "react";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  // Cmd + Jでライトモード/ダークモードを切り替え
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
 
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
