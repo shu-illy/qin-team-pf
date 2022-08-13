a692a7f25dc8b6e330fdeb33027edf14b9f115ae
shu-illy <sh.illy.9117@gmail.com>
2022-08-14 06:24:18 +0900

    絶対パスに修正

--- src/lib/mantine/index.ts
+++ src/lib/mantine/index.ts
@@ -1,4 +1,4 @@
-export { useTextColor } from "./useTextColor";
-export { useBackgroundColor } from "./useBackgroundColor";
-export { Button } from "./Button";
-export { useMediaQuery } from "./useMediaQuery";
+export { useTextColor } from "lib/mantine/useTextColor";
+export { useBackgroundColor } from "lib/mantine/useBackgroundColor";
+export { Button } from "lib/mantine/Button";
+export { useMediaQuery } from "lib/mantine/useMediaQuery";
