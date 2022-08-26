da51b5a5abc0e2a75bbc5ae90831b5cbacf4319a
shu-illy <sh.illy.9117@gmail.com>
2022-08-27 06:30:48 +0900

    環境変数の表記を変更

--- src/lib/microcms/client.ts
+++ src/lib/microcms/client.ts
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const microCmsClient = createClient({
   serviceDomain: "dunqzqec2e",
-  apiKey: process.env.API_KEY ?? "",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY ?? "",
 });
