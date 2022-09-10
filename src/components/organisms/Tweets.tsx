bebc113066ef68786273c7c8c072ac55d92e16a6
shu-illy <sh.illy.9117@gmail.com>
2022-09-11 07:38:51 +0900

    UI調整

--- src/components/organisms/Tweets.tsx
+++ src/components/organisms/Tweets.tsx
@@ -1,7 +1,8 @@
-import { Center, Space, Stack } from "@mantine/core";
+import { Center, ScrollArea, Space, Stack } from "@mantine/core";
 import SectionBottomButton from "components/atoms/SectionBottomButton";
 import { SectionTitle } from "components/atoms/SectionTitle";
 import TweetItem from "components/molecules/TweetItem";
+import { useMediaQuery } from "lib/mantine";
 import React, { FC } from "react";
 import { Tweet } from "types";
 
@@ -10,16 +11,19 @@ type Props = {
 };
 
 const Tweets: FC<Props> = ({ tweets }) => {
-  const items = tweets.slice(0, 3);
+  const isDesktop = useMediaQuery("sm");
   return (
     <Stack spacing={0}>
       <SectionTitle>Twitter</SectionTitle>
-      <Stack spacing={56}>
-        {items.map((tweet) => (
-          <TweetItem key={tweet.id} tweet={tweet} />
-        ))}
-      </Stack>
-      <Space h={40} />
+      <ScrollArea style={{ height: isDesktop ? 880 : 400 }}>
+        <Stack spacing={24}>
+          {tweets.map((tweet) => (
+            <TweetItem key={tweet.id} tweet={tweet} />
+          ))}
+        </Stack>
+      </ScrollArea>
+
+      <Space h={24} />
       <Center mt={8}>
         <SectionBottomButton label="View on Twitter" />
       </Center>
