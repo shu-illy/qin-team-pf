68ba037550373f9d0b8c8a604c23f2d355ce91c1
shu-illy <sh.illy.9117@gmail.com>
2022-09-11 08:16:54 +0900

    test

--- src/pages/api/user-tweets/[id].ts
+++ src/pages/api/user-tweets/[id].ts
@@ -46,6 +46,7 @@ export default async function handler(
   //   },
   // ]);
   try {
+    console.log(userId);
     const twitterResponse = await twitterClient.tweets.usersIdTweets(userId, {
       expansions: ["author_id"],
       "tweet.fields": ["author_id", "created_at"],
