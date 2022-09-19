4489096acb46201de20522744c02383da70f2045
shu-illy <sh.illy.9117@gmail.com>
2022-09-19 17:38:55 +0900

    graphql-codegenで型生成

---
+++ src/lib/github/query.graphql.ts
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const repositoryLanguagesQuery = gql`query GetRepositoryLanguages($repositoriesFirst: Int, $languagesFirst: Int) {
+  viewer {
+    repositories(first: $repositoriesFirst, orderBy: { field: PUSHED_AT, direction: DESC }) {
+      nodes {
+        id
+        name
+        forkCount
+        stargazerCount
+        languages(first: $languagesFirst) {
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+          }
+          totalSize
+          totalCount
+          edges {
+            cursor
+            node {
+              id
+              name
+              color
+            }
+            size
+          }
+        }
+      }
+    }
+  }
+}
+`;
