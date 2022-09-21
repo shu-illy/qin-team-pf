828d0fada4a2c5c64707697f1f624c306f855666
shu-illy <sh.illy.9117@gmail.com>
2022-09-21 16:24:30 +0900

    CSRを実装

--- src/components/organisms/GithubRepositories.tsx
+++ src/components/organisms/GithubRepositories.tsx
@@ -1,16 +1,30 @@
-import { Center, Stack } from "@mantine/core";
+import { useQuery } from "@apollo/client";
+import { Center, Stack, Text } from "@mantine/core";
 import SectionBottomButton from "components/atoms/SectionBottomButton";
 import { SectionTitle } from "components/atoms/SectionTitle";
 import RepositoryItem from "components/molecules/RepositoryItem";
+import { repositoryLanguagesQuery } from "lib/github/query.graphql";
 import React, { FC } from "react";
 import { GithubRepository } from "types";
+import { GetRepositoryLanguagesQuery } from "types/github";
+import { queryToRepositories } from "utils/repositoriesQueryConverter";
 
 type Props = {
   repositories: GithubRepository[];
 };
 
 const GithubRepositories: FC<Props> = ({ repositories }) => {
-  const items = repositories.slice(0, 5);
+  const { data } = useQuery<GetRepositoryLanguagesQuery>(repositoryLanguagesQuery, {
+    variables: { repositoriesFirst: 10, languagesFirst: 10 },
+  });
+
+  const items: GithubRepository[] = [];
+  if (data) {
+    Array.prototype.push.apply(items, queryToRepositories(data));
+  } else {
+    Array.prototype.push.apply(items, repositories);
+  }
+
   return (
     <Stack spacing={0}>
       <SectionTitle>GitHub</SectionTitle>
